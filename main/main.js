72e0bd208d52100bf4350ccc7f9e4414c6a58e4d
swords-king <zzy11235@qq.com>
2018-09-23 22:22:18 +0800

    homework for 2_2

--- main/main.js
+++ main/main.js
@@ -1,18 +1,42 @@
 module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
     console.log(`o) 最小值 = ${sequence.minimum()}
-// Write your code here
-`);
+o) 最大值 = ${sequence.maxmum()}
+o) 元素数量 = ${sequence.itemcount()}
+o) 平均值 = ${sequence.avg()}`);
 };
 
 class Sequence {
   constructor(input) {
-    // Write your code here
+    this.numlist = input
   }
 
   minimum() {
     // Write your code here
+	var minnum = this.numlist[0];
+	for (var i=1; i<this.numlist.length;i++)
+		if(minnum > this.numlist[i]){
+			minnum = this.numlist[i];}
+	return minnum;
   }
 
   // Write your code here
+  maxmum() {
+	  var maxnum = this.numlist[0];
+	  for (var i=1; i<this.numlist.length;i++)
+		  if(maxnum < this.numlist[i]){
+				maxnum = this.numlist[i];}
+	  return maxnum;
+  }
+  itemcount() {
+	  return this.numlist.length;
+  }
+  avg() {
+	  var sum = 0;
+	  for (var x of this.numlist){
+		sum += x;
+	  }
+	  var avgs = sum/this.numlist.length
+	  return avgs.toFixed(2);
+  }
 }
